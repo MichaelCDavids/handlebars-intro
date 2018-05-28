3901859f48bfa193361ab423e63149388c5071fd
michael <michaelcdavids@gmail.com>
2018-05-28 11:13:16 +0200

    added test for the color change function

---
+++ dom-intro-templates/test/factory-functions-test.js
@@ -0,0 +1,65 @@
+describe('The colorChange function ',function(){
+
+  it('should return the name of the class to be added when warning level is reached', function(){
+     var textFactoryObject = TextBill();
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+
+
+     assert.equal(textFactoryObject.thresholdColor(),"warning");
+  });
+
+  it('should return the name of the class to be added when warning level is reached', function(){
+     var textFactoryObject = TextBill();
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('call');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+     textFactoryObject.billTwo('sms');
+
+
+     assert.equal(textFactoryObject.thresholdColor(),"danger");
+  });
+
+});
